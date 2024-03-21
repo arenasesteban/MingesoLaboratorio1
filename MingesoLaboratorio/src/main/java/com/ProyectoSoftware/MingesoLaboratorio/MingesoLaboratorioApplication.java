5a2d539c1fdca4b233d6a3fe5693b4ce6d54062d
arenasesteban <esteban.arenas.a@usach.cl>
2024-03-21 09:49:48 -0300

    Carga proyecto con Spring Boot Maven

---
+++ MingesoLaboratorio/src/main/java/com/ProyectoSoftware/MingesoLaboratorio/MingesoLaboratorioApplication.java
@@ -0,0 +1,13 @@
+package com.ProyectoSoftware.MingesoLaboratorio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MingesoLaboratorioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MingesoLaboratorioApplication.class, args);
+	}
+
+}
---
+++ MingesoLaboratorio/src/test/java/com/ProyectoSoftware/MingesoLaboratorio/MingesoLaboratorioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ProyectoSoftware.MingesoLaboratorio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MingesoLaboratorioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
