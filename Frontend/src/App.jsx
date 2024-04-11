e3d6fa04d3b827fbaf167266fc5d6200b7fd91df
arenasesteban <esteban.arenas.a@usach.cl>
2024-04-11 11:53:38 -0400

    [Esteban] Trabajo front-end - SideBar creada con nombre de paginas

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,16 +1,22 @@
 import './App.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import SideBar from './components/SideBar'
 import NavBar from './components/NavBar';
 
-function App() {
+export default function App() {
     return (
-        <BrowserRouter>
-            <NavBar/>
-            <Routes>
-                <Route></Route>
-            </Routes>
-        </BrowserRouter>
+        <div className="grid grid-cols-6 h-screen">
+            <div className="col-span-1">
+                <SideBar />
+            </div>
+                <div className="col-span-5">
+                    <div className="">
+                        {/* NavBar */}
+                    </div>
+                    <div className="">
+                        {/* Contenido */}
+                </div>
+            </div>
+        </div>
     )
 }
-
-export default App
