fe1f6ceb05d03b042382e2941e2c440b00ba84d8
arenasesteban <esteban.arenas.a@usach.cl>
2024-04-24 23:37:37 -0400

    [Esteban] Cambio en ruteo sidebar, ahora utiliza navigate

--- Frontend/src/components/OpcionSideBar.jsx
+++ Frontend/src/components/OpcionSideBar.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
-export default function OpcionSideBar({ enlace, nombreOpcion, iconoOpcion, iconoDropdown}) {
+export default function OpcionSideBar({ nombreOpcion, iconoOpcion, iconoDropdown, onClick}) {
 
     return (
         <div className="mx-3 mt-auto">
-            <a href={enlace} className="flex gap-3 items-center px-4 py-3 text-white font-semibold rounded-lg hover:bg-cyan-700 hover:shadow-md active:text-cyan-600 active:bg-white">
+            <button onClick={onClick} className="flex gap-3 items-center w-full px-4 py-3 text-white font-semibold rounded-lg hover:bg-cyan-700 hover:shadow-md active:text-cyan-600 active:bg-white">
                 {iconoOpcion && <span>{iconoOpcion}</span>}
                 {nombreOpcion}
                 {iconoDropdown && <span>{iconoDropdown}</span>}
-            </a>
+            </button>
         </div>
     );
 }
